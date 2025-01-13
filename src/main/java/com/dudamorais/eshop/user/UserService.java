aec205d4a70b9b1f20d59e58191993c51e65a0f2
AndDev741 <anddesenvolved@gmail.com>
2025-01-13 13:20:56 -0300

    fix: Remove unused dependencie from User entity

--- src/main/java/com/dudamorais/eshop/user/UserService.java
+++ src/main/java/com/dudamorais/eshop/user/UserService.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
